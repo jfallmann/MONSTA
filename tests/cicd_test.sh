b830d86dde6687ef284dd18f6e929cc49039bffa
jfallmann <fallmann.joerg@gmail.com>
2024-12-17 15:16:10 +0100

    test env and python-app update

--- tests/cicd_test.sh
+++ tests/cicd_test.sh
@@ -2,6 +2,7 @@
 
 # Get the directory of the current script
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
+echo ${SCRIPT_DIR}
 
 # Run the MONSDA command with the updated -c parameter
-monsda -j 6 -c "${SCRIPT_DIR}/cicd_test.sh" --directory "${SCRIPT_DIR}" --use-apptainer --apptainer-prefix "${SCRIPT_DIR}/Containers" --rerun-incomplete --rerun-triggers mtime
+monsda -j 6 -c "${SCRIPT_DIR}/multitool.json" --directory "${SCRIPT_DIR}" --use-apptainer --apptainer-prefix "${SCRIPT_DIR}/Containers" --rerun-incomplete --rerun-triggers mtime
