face6f5c5460c3595f73cc514f71a5fbb91c5608
jfallmann <fallmann.joerg@gmail.com>
2025-01-27 14:03:21 +0100

    test updates

--- tests/cicd_test.sh
+++ tests/cicd_test.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-conda activate monsda-test
+#conda activate monsda-test
 
 # Get the directory of the current script
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 echo ${SCRIPT_DIR}
-
+cd ${SCRIPT_DIR}
+mkdir -p CONDALIB
 # Run the MONSDA command with the updated -c parameter
-monsda -j 6 -c "${SCRIPT_DIR}/data/config_Test.json" --directory "${SCRIPT_DIR}" --use-apptainer --apptainer-prefix "${SCRIPT_DIR}/Containers" --rerun-incomplete --rerun-triggers mtime
+monsda -j 6 -c config_Test.json --directory ${PWD} --use-conda --conda-prefix CONDALIB --rerun-incomplete --rerun-triggers mtime && echo "MONSDA test passed" || echo "MONSDA test failed"
